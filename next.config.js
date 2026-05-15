ca98ee8936b90b3cfe38797eba6f89055ae66441
paintio <esygameengine@gmail.com>
2026-05-15 19:33:44 +0300

    Fix: replace next.config.ts with next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'lh3.googleusercontent.com'],
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
